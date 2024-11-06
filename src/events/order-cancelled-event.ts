74e261cf87790fc4dff0b4525fc1bee7030baf73
Colson <colsoncalifornication@gmail.com>
2024-11-06 17:20:34 +0545

    resolve version in event

--- src/events/order-cancelled-event.ts
+++ src/events/order-cancelled-event.ts
@@ -3,10 +3,10 @@ import { Subjects } from './subjects';
 // Event to describe the order that was just cancelled
 export interface OrderCancelledEvent {
   subject: Subjects.OrderCancelled;
-  version: number;
   data: {
     // id of the order
     id: string;
+    version: number;
     // id of the ticket that will be marked as being unreserved now
     ticket: {
       id: string;
